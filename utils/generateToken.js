093e2e4239a75885246518959c53c0824782dfc3
Hadiur <fmhadiuzzaman@gmail.com>
2025-06-07 00:40:28 +0200

    solved user update problem

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,17 +1,7 @@
 const jwt = require("jsonwebtoken");
 
-const generateToken = (user) => {
-  return jwt.sign(
-    {
-      _id: user._id,
-      role: user.role,
-      isVerified: user.isVerified,
-      name: user.name,
-      email: user.email,
-    },
-    process.env.JWT_SECRET,
-    { expiresIn: "7d" }
-  );
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 };
 
 module.exports = generateToken;
