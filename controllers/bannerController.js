4f5f18cd2c1a292a7f9acfe393c07a67548fc80c
Hadiur <fmhadiuzzaman@gmail.com>
2025-06-23 21:21:05 +0200

    Added Banner section

--- controllers/bannerController.js
+++ controllers/bannerController.js
@@ -1,5 +1,14 @@
-// controllers/bannerController.js
 const Banner = require("../models/Banner");
+
+exports.getBanners = async (req, res) => {
+  try {
+    const banners = await Banner.find().sort({ createdAt: -1 }); // Optional: latest first
+    res.json(banners);
+  } catch (err) {
+    console.error("Failed to get banners:", err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
 
 exports.addBanner = async (req, res) => {
   const { imageUrl, caption, link } = req.body;
