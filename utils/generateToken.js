e0a3b99914874f67452462730bcad4ed0bd7bec2
Hadiur <fmhadiuzzaman@gmail.com>
2025-07-31 02:45:25 +0200

    Added role switch function

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,9 +1,20 @@
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 
+// const generateToken = (user) => {
+//   return jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+//     expiresIn: "30d", // or "7d" if you prefer
+//   });
+// };
+
+// module.exports = generateToken;
 const generateToken = (user) => {
-  return jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: "30d", // or "7d" if you prefer
-  });
+  return jwt.sign(
+    {
+      id: user._id,
+      roles: user.roles, // optionally include roles
+      primaryRole: user.primaryRole,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "30d" }
+  );
 };
-
-module.exports = generateToken;
